18ab51a061fc12bbdf8295f16bf455855fbd2547
김민수 MINSOO KIM <alstn113@gmail.com>
2023-09-12 10:38:30 +0900

    fix: nextjs 13.4 socket config error

--- packages/web/src/app/(dashboard)/layout.tsx
+++ packages/web/src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
-export default function DashBoardLayout() {
-  return <div>DashBoardLayout</div>;
+export default function DashBoardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
 }
