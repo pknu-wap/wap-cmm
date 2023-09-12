8de4da202945c52d5efaea3747557b411a8759f8
김민수 MINSOO KIM <alstn113@gmail.com>
2023-09-12 14:20:21 +0900

    feat: server prisma user model

--- packages/server/src/modules/users/users.module.ts
+++ packages/server/src/modules/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UsersRepository],
   exports: [],
 })
 export class UsersModule {}
