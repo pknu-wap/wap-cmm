21759bb9cec16a114c91ede1abe2fb592e1456c6
김민수 MINSOO KIM <alstn113@gmail.com>
2023-09-19 23:24:27 +0900

    chore: remove unused modules

--- packages/web/.lintstagedrc.js
+++ packages/web/.lintstagedrc.js
@@ -1,16 +1,14 @@
-const path = require('path');
-
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
-
-const buildPrettierCommand = (filenames) =>
-  `prettier --write ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' ')}`;
-
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-  '*.{json,md,mdx,css,html,yml}': [buildPrettierCommand],
+  // Type check TypeScript files
+  '**/*.(ts|tsx)': () => 'pnpm tsc --noEmit',
+
+  // Lint & Prettify TS and JS files
+  '**/*.(ts|tsx|js)': (filenames) => [
+    `next lint ${filenames.join(' ')}`,
+    `pnpm prettier --write ${filenames.join(' ')}`,
+  ],
+
+  // Prettify only Markdown and JSON files
+  '**/*.(md|json)': (filenames) =>
+    `pnpm prettier --write ${filenames.join(' ')}`,
 };
